0c834a49e104d89d7d70e51604603dce13e45d0e
aidansunbury <aidansunbury@gmail.com>
2022-10-24 13:55:36 -0700

    finished hw

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -1,23 +1,28 @@
-import { useState } from 'react';
-import React from 'react';
-import Menu from './Menu';
-import Block from './Block';
+import { useState } from "react";
+import React from "react";
+import Menu from "./Menu";
+import Block from "./Block";
 
 const Feed = () => {
-    /* TODO: Declare a new state variable to keep track of the blocks on your Blockstagram feed! */
-    // Refer to Hint 2 for more help!
+  /* TODO: Declare a new state variable to keep track of the blocks on your Blockstagram feed! */
+  // Refer to Hint 2 for more help!
 
-    /* Use the map() function to render multiple Blocks! */
-    const posts = null; // TODO: edit this variable
+  /* Use the map() function to render multiple Blocks! */
+  // TODO: edit this variable
 
-    return (
-        <div>
-            <Menu></Menu>
+  const [posts, setPosts] = useState([]);
 
-            {/* Below is where all of your Blocks should render! */}
-            {posts}
-        </div>
-    );
-}
+  const createPost = (postColor) => {
+    setPosts([<Block color={postColor}></Block>, ...posts]); //array destructuring is more fun when you actually understand it
+    console.log("CreatePost called with post color: ", postColor);
+    console.log("There are ", posts.length, " posts.");
+  };
+  return (
+    <div>
+      <Menu click={createPost}></Menu>
+      {posts} {/**Chefs kiss */}
+    </div>
+  );
+};
 
 export default Feed;
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React from "react";
+
+import Color from "./Color";
+
 /* Add any imports you think you might need here! */
-
-const Menu = () => { 
-
-    return (
-      <div className="colorOptions">
-          {/* TODO */}
-      </div>
-    );
-}
+//Thank you: https://stackoverflow.com/questions/52792923/react-can-you-pass-state-handler-down-multiple-child-component-layers
+const Menu = ({ click }) => {
+  return (
+    <div className="colorOptions">
+      <Color color="red" handleClick={click}></Color>
+      <Color color="pink" handleClick={click}></Color>
+      <Color color="blue" handleClick={click}></Color>
+      <Color color="green" handleClick={click}></Color>
+    </div>
+  );
+};
 
 export default Menu;
